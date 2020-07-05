fc305b1e0edd34ce60828d469f542581ecca02ae
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-07-06 02:34:43 +0530

    Adding correct folder structure

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def my_form():
+    return render_template('/index.html')   
+
+if __name__ == "__main__":      
+    app.run() 
