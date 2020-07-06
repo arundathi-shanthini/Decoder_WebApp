d26b34cff945db8d930a9f13fdab75b6c1fd16d8
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-07-06 18:56:58 +0530

    Made t2b to work

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, Markup
 app = Flask(__name__, static_folder='templates/stylesheets')
+app.secret_key = "super secret key"
 
 @app.route('/')
 def render_index_page():
@@ -7,17 +8,20 @@ def render_index_page():
 
 @app.route('/t2b', methods=['POST', 'GET'])
 def render_t2b():
-    return render_template('/t2b.html')
+	if request.method == 'GET':
+		return render_template('/t2b.html')
+	if request.method == 'POST':
+		message=request.form['encodedMessage']
+		if message:
+			converted_message="Testing"
+			return render_template("/t2b-solve.html", input=message, output=converted_message)
+		else:
+			return render_template('/t2b.html')
 
-def encode_message():
-	return
-	
 @app.route('/b2t', methods=['POST', 'GET']) 
 def render_b2t():
     return render_template('/b2t.html')   
 
-def decode_message():
-	return
-
-if __name__ == "__main__":      
-    app.run() 
+if __name__ == "__main__":   
+	app.debug=True
+	app.run()
--- test_files/Binary_to_ASCII.py
+++
@@ -1,9 +0,0 @@
-# Read the message
-with open("message_to_encode.txt") as f:
-    encoded_message=f.read().split()
-
-# Decode the message
-decoded_message = ''.join([chr(int(letter,2)) for letter in encoded_message])
-
-# Display the message
-print(decoded_message)
