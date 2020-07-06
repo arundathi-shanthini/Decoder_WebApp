2bd95d3eaa19be8852b94e70afe73cf73e371f18
Arundathi Shaji Shanthini <arundathisanthini@gmail.com>
2020-07-06 11:02:58 +0530

    Wokring with correct navigation

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, request, render_template
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates/stylesheets')
 
 @app.route('/')
-def my_form():
-    return render_template('/index.html')   
+def render_index_page():
+    return render_template('/index.html')
+
+@app.route('/t2b', methods=['POST', 'GET'])
+def render_t2b():
+    return render_template('/t2b.html')
+
+def encode_message():
+	return
+	
+@app.route('/b2t', methods=['POST', 'GET']) 
+def render_b2t():
+    return render_template('/b2t.html')   
+
+def decode_message():
+	return
 
 if __name__ == "__main__":      
     app.run() 
